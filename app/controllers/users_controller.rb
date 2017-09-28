5985ad2ab80753ec80396a6bc52d99fa55a33e1d
Peter Marcano <pbmarcano91@gmail.com>
2017-09-28 17:52:08 +0100

    Added authentication and such

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,13 @@
+class UsersController < Clearance::UsersController
+  # def new
+  #   @user = User.new
+  #   render template: "users/new"
+  # end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
+end
