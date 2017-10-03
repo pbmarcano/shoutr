f5fb2d6c0b1d7200cf15d2c5318768d747348658
Peter Marcano <pbmarcano91@gmail.com>
2017-10-03 16:07:20 -0400

    Fixed sign up and allowing null in shouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,19 @@
 class UsersController < Clearance::UsersController
+
+  def new
+    @user = User.new 
+  end 
+
   def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
   end
-  
+
   private
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
   end
+
 end
 
